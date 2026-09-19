f26ac96115bc34480852f2b67049b97321b30172
radenrifqi_ <radenrifqi_@gmail.com>
2026-09-19 10:43:12 +0700

    Add database health check

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/health-db', function () {
+    $database = DB::selectOne('SELECT DATABASE() AS database_name');
+
+    return response()->json([
+        'status' => true,
+        'message' => 'Laravel dan MySQL berhasil terhubung',
+        'database' => $database->database_name,
+    ]);
 });
